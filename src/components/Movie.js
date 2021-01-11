ab76580f961e2a8f3d0d2856187bf67a05e03548
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-01-11 22:21:11 +0530

    added deafault image

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -14,7 +14,7 @@ const setVoteClass = (vote => {
 
 const Movie = ({ title, poster_path, overview, vote_average }) => (
     <div className="movie">
-        <img src={ IMG_API + poster_path} alt={title} />
+        <img src={(poster_path ? IMG_API + poster_path : 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1040&q=80')} alt={title} />
         <div className="movie-info">
             <h3>{title}</h3>
             <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
